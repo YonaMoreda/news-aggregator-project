6740d4832bf41521e6729ae25bb6fbab18ab2536
YonaMoreda <yokots3q@gmail.com>
2020-11-26 11:02:33 +0100

    First refactor, hello world page

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import logo from '../Resources/logo.svg';
+import '../Stylesheets/App.css';
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Start of project
+            </header>
+        </div>
+    );
+}
+
+export default App;

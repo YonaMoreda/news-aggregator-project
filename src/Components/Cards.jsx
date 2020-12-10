a423ac9ea82dabeea74ef8f05f7555ec476d25c3
YonaMoreda <yokots3q@gmail.com>
2020-12-10 21:42:12 +0100

    logos when images are missing added (refactor)

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,9 +1,21 @@
 import React, {useEffect, useState} from "react";
 
-const img =
-    "https://www.dw.com/cssi/dwlogo-print.gif";
+const img_dict = {
+    "www.dw.com": "https://www.dw.com/cssi/dwlogo-print.gif",
+    "amharic.voanews.com": "https://amharic.voanews.com/Content/responsive/VOA/am-ET/img/logo.png"
+};
 
-
+function getHostname(url) {
+    let hostname;
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    } else {
+        hostname = url.split('/')[0];
+    }
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+    return hostname;
+}
 function Cards(props) {
     const [news, setNews] = useState([]);
     const getNews = async () => {
@@ -29,7 +41,7 @@ function Cards(props) {
                                 <a href={link} target="_blank" rel="noopener noreferrer">
                                     <div className="card">
                                         <img
-                                            src={`${enclosures ? enclosures[0].url : img}`}
+                                            src={`${enclosures ? enclosures[0].url : img_dict[getHostname(link)]}`}
                                             alt={title}
                                         />
                                         <h1>{title}</h1>

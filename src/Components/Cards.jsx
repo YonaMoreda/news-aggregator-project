58ea91762e72e7482141f7195429250453328233
Dave <Dwtmekonnen123@gmail.com>
2020-12-11 15:21:12 +0300

    added error handling and loader

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,59 +1,85 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "./loader";
 
 const img_dict = {
-    "www.dw.com": "https://www.dw.com/cssi/dwlogo-print.gif",
-    "amharic.voanews.com": "https://amharic.voanews.com/Content/responsive/VOA/am-ET/img/logo.png"
+  "www.dw.com": "https://www.dw.com/cssi/dwlogo-print.gif",
+  "amharic.voanews.com":
+    "https://amharic.voanews.com/Content/responsive/VOA/am-ET/img/logo.png",
 };
 
 function getHostname(url) {
-    let hostname;
-    if (url.indexOf("//") > -1) {
-        hostname = url.split('/')[2];
-    } else {
-        hostname = url.split('/')[0];
-    }
-    hostname = hostname.split(':')[0];
-    hostname = hostname.split('?')[0];
-    return hostname;
+  let hostname;
+  if (url.indexOf("//") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+  hostname = hostname.split(":")[0];
+  hostname = hostname.split("?")[0];
+  return hostname;
 }
-function Cards(props) {
-    const [news, setNews] = useState([]);
-    const getNews = async () => {
-        try {
-            const response = await fetch(props.url);
-            return await response.json();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    useEffect(() => {
-        getNews().then(r => setNews(r));
-    }, []);
-    let news_items = news.items;
-    return (
-        <>
-            {news_items &&
-                news_items.map((item) => {
-                    const {title, description, link, enclosures} = item;
-                    if (description) {
-                        return (
-                            <>
-                                <a href={link} target="_blank" rel="noopener noreferrer">
-                                    <div className="card">
-                                        <img
-                                            src={`${enclosures ? enclosures[0].url : img_dict[getHostname(link)]}`}
-                                            alt={title}
-                                        />
-                                        <h1>{title}</h1>
-                                        <p>{description.substring(0, 200)}</p>
-                                    </div>
-                                </a>
-                            </>
-                        );
-                    }
-                })}
-        </>
-    );
+function Cards({ url }) {
+  const [news, setNews] = useState([]);
+  const [loading, setLoader] = useState(true);
+  const [error, setError] = useState(false);
+  const getNews = async () => {
+    try {
+      const response = await fetch(url);
+      if (response.status <= 200 || response.status >= 399) {
+        const data = await response.json();
+        setNews(data);
+        setLoader(false);
+        setError(false);
+      } else {
+        setLoader(false);
+        setError(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoader(false);
+
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    getNews();
+  }, []);
+  let news_items = news.items;
+  if (loading) {
+    return <Loader />;
+  }
+  if (error) {
+    return <h1>error ...</h1>;
+  }
+  return (
+    <>
+      {news_items &&
+        news_items.map((item, index) => {
+          const { title, description, link, enclosures } = item;
+          if (description) {
+            return (
+              <div key={index}>
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                  <div className="card">
+                    <img
+                      src={`${
+                        enclosures
+                          ? enclosures[0].url
+                          : img_dict[getHostname(link)]
+                      }`}
+                      alt={title}
+                    />
+                    <h1>{title}</h1>
+
+                    <p>{description.substring(0, 200)}</p>
+                  </div>
+                </a>
+              </div>
+            );
+          }
+        })}
+    </>
+  );
 }
 
 export default Cards;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 import "../Stylesheets/home.css";
 import Cards from "./Cards";
-
 const url2 =
-    " https://send-rss-get-json.herokuapp.com/convert/?u=https://rss.dw.com/xml/rss-amh-news";
+  " https://send-rss-get-json.herokuapp.com/convert/?u=https://rss.dw.com/xml/rss-amh-news";
 const url1 =
-    "https://send-rss-get-json.herokuapp.com/convert/?u=https://amharic.voanews.com/api/epiqq";
+  "https://send-rss-get-json.herokuapp.com/convert/?u=https://amharic.voanews.com/api/epiqq";
 
 function Home() {
-    return (
-        <>
-            <nav className="nav">
-                <h2>የዜና አርማ</h2>
-                <h2>ያስሱ</h2>
-                <h2>ዋና ዜና</h2>
-                <h2>ርዕሶች</h2>
-            </nav>
-            <header>አሁን በመታየት ላይ</header>
-            <h2 className="CardHeader">የአሜሪካ ድምፅ (ቪኦኤ)</h2>
-            <section>
-                <Cards url={url1}/>
-            </section>
-            <h2 className="CardHeader">DW ሬዲዮ</h2>
-            <section>
-                <Cards url={url2}/>
-            </section>
-            <footer>የተወሰነ ቦታ ያዥ - ፪፼፲፫</footer>
-        </>
-    );
+  return (
+    <>
+      <nav className="nav">
+        <h2>የዜና አርማ</h2>
+        <h2>ያስሱ</h2>
+        <h2>ዋና ዜና</h2>
+        <h2>ርዕሶች</h2>
+      </nav>
+      <header>አሁን በመታየት ላይ</header>
+      <h2 className="CardHeader">የአሜሪካ ድምፅ (ቪኦኤ)</h2>
+      <section>
+        <Cards url={url1} />
+      </section>
+      <h2 className="CardHeader">DW ሬዲዮ</h2>
+      <section>
+        <Cards url={url2} />
+      </section>
+      <footer>የተወሰነ ቦታ ያዥ - ፪፼፲፫</footer>
+    </>
+  );
 }
 
 export default Home;
